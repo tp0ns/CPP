0b7be51b7d55d1683047595d9c5c3dc797db4456
tp0ns <tpons@student.42.fr>
2022-01-21 12:58:29 +0100

    01/ex01

--- 01/ex01/main.cpp
+++ 01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 10:34:56 by tpons             #+#    #+#             */
-/*   Updated: 2022/01/14 10:48:05 by tpons            ###   ########.fr       */
+/*   Updated: 2022/01/21 12:57:22 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ Zombie*	zombieHorde( int N, std::string name );
 
 int	main( void ) {
 	Zombie* horde;
+	int		hordeSize = 5;
 
-	horde = zombieHorde(5, "Morty");
-	for (int i = 0; i < 5; i++)
+	horde = zombieHorde(hordeSize, "Morty");
+	for (int i = 0; i < hordeSize; i++)
 		horde[i].announce();
 	delete[] horde;
 	return (0);
