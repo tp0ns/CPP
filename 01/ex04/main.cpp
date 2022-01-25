16225ae37c109dcbf5c4ade46fd5bae7a5e88e00
tp0ns <tpons@student.42.fr>
2022-01-25 12:37:43 +0100

    fixed compilation with 98

--- 01/ex04/main.cpp
+++ 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 12:43:59 by tpons             #+#    #+#             */
-/*   Updated: 2022/01/21 14:40:15 by tpons            ###   ########.fr       */
+/*   Updated: 2022/01/25 11:55:16 by tpons            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@ std::string	ft_replace(std::string temp, size_t pos, size_t len, std::string s2)
 
 int	sed_for_losers(char **av)
 {
-	std::string 		filename = av[1], s1 = av[2], s2 = av[3];
-	std::ifstream		inputfile(filename);
+	std::string 		filename = av[1], out_filename = (filename + ".replace"), s1 = av[2], s2 = av[3];
+	std::ifstream		inputfile(filename.c_str());
 	std::stringstream	temp_stream;
 	std::string			temp;
-	
+	std::ofstream		outputfile(out_filename.c_str());
+
 	if (filename.empty() || s1.empty())
 	{
 		std::cout<< "One of your argument is invalid" <<std::endl;
 		return (0);
 	}
-	std::ofstream		outputfile(filename + ".replace");
 	if (inputfile.is_open() && outputfile.is_open())
 	{
 		temp_stream << inputfile.rdbuf();
