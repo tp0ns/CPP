1caa04250408ca4d2477c5555d34461bd88d1b39
tp0ns <tpons@student.42.fr>
2022-01-28 16:00:38 +0100

    03/ex01

---
+++ 03/ex01/main.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpons <tpons@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 14:42:39 by tpons             #+#    #+#             */
+/*   Updated: 2022/01/28 15:25:21 by tpons            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
+int main( void ) {
+		ClapTrap clappy("Clappy");
+	
+    	std::cout<<std::endl;
+		clappy.takeDamage(2);
+		clappy.attack("coucou");
+    	std::cout<<std::endl;
+		
+		ClapTrap copy(clappy);
+	
+    	std::cout<<std::endl;
+		copy.setName("Copy");
+		copy.setDamage(5);
+		copy.attack("Michael Scott");
+    	std::cout<<std::endl;
+		
+		ScavTrap scavy("Scavy");
+
+	    std::cout<<std::endl;
+		scavy.guardGate();
+		scavy.takeDamage(2);
+		scavy.attack("Dwight Schrute");
+	    std::cout<<std::endl;
+
+		ScavTrap scopy(scavy);
+
+	    std::cout<<std::endl;
+		scopy.setName("Scopy");
+		scopy.setDamage(5);
+		scopy.attack("Michael Scott");
+	    std::cout<<std::endl;
+	return (0);
+}
---
+++ 03/ex01/sources.mk
@@ -0,0 +1,10 @@
+## SOURCES.MK ##
+
+## . ##
+
+SOURCES		+=           		\
+		ScavTrap.cpp        	\
+		ClapTrap.cpp        	\
+		main.cpp            	\
+
+
