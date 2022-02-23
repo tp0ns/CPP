94e7e735f1251e60458b766ec02d1c57e17379f3
tp0ns <tpons@student.42.fr>
2022-02-23 08:06:09 +0100

    08/ex02

---
+++ 08/ex02/MutantStack.hpp
@@ -0,0 +1,15 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <string>
+# include <stack>
+# include <algorithm>
+
+template <typename T>
+class MutantStack : public std::stack< T >
+{
+	
+};
+
+#endif /* ***************************************************** MUTANTSTACK_H */
